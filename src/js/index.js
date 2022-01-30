2324d541587894e12d1a89579f3d03b208b62418
vovasap <vovasap.pblc@gmail.com>
2022-01-30 18:28:22 +0300

    feat: add mobile display for buckets

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ function addBuckets() {
   const bucketsElement = document.querySelector('.buckets')
   const bucketTemplate = document.querySelector('#bucket')
 
-  bucketsTypes.forEach(type => {
+  bucketsTypes.forEach((type, index) => {
     const bucketClone = bucketTemplate.content.cloneNode(true)
 
     const bucketLabel = bucketClone.querySelector('.bucket__label')
@@ -17,9 +17,11 @@ function addBuckets() {
     bucketLabel.textContent = type.name
     bucketLabel.style.backgroundColor = type.color
 
+    const bucketContainer =  bucketClone.querySelector('.bucket__container')
     const bucketTop =  bucketClone.querySelector('.bucket__top')
     const bucketBase =  bucketClone.querySelector('.bucket__base')
 
+    bucketContainer.style.zIndex = 100 - index
     bucketTop.setAttribute('src', `../assets/images/wastetop_${type.color}.jpg`)
     bucketTop.setAttribute('alt', `wastetop_${type.color}`)
     bucketBase.setAttribute('src', `../assets/images/wastebox_${type.color}.jpg`)
