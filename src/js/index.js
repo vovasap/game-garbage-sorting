ad9783cb9bc25936e832d516611396c6a7361ab5
vovasap <vovasap.pblc@gmail.com>
2022-01-31 05:03:04 +0300

    feat: add close button

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,12 @@ function init() {
   addBuckets()
 
   const startButton = document.querySelector('.timer__start-button')
+  const closeButton = document.querySelector('.game__close')
   
   startButton.addEventListener('click', startTimer)
+  closeButton.addEventListener('click', () => {
+    console.log('close')
+  })
 }
 
 init()
